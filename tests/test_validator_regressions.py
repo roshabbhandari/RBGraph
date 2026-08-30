bc37c5c1b27bebd6a7feca86db1aa0b3516d4de9
Roshab Bhandari <roshabbhandari1334@gmail.com>
2026-08-30 19:09:49 +0545

    test: reject malformed SVG viewBox values

--- tests/test_validator_regressions.py
+++ tests/test_validator_regressions.py
@@ -1,5 +1,5 @@
 from core.models import Diagram, DiagramType, Edge, Node
-from core.validator import validate_diagram
+from core.validator import validate_diagram, validate_svg
 
 
 def test_self_referencing_edge_is_rejected():
@@ -29,3 +29,11 @@ def test_duplicate_connections_are_rejected_even_with_labels():
     errors = validate_diagram(diagram)
 
     assert any("duplicate" in error.lower() for error in errors)
+
+
+def test_invalid_svg_viewbox_is_rejected():
+    svg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 nope 900 500"><rect width="900" height="500"/></svg>'
+
+    errors = validate_svg(svg)
+
+    assert any("viewbox" in error.lower() for error in errors)
